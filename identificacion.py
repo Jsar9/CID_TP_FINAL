9c8e6213e8c1fd37fff7acbfb8bae9f38f3d34b3
Jorge Alfredo Sarmiento <jsarmiento@fi.uba.ar>
2026-07-01 18:07:54 -0300

    se agregó identificacion.py (incompleto) y curva_tacometro.png

---
+++ identificacion.py
@@ -0,0 +1,46 @@
+#Librerias
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+#Funciones 
+
+
+
+
+
+
+#------Transformación de los datos
+
+#Tiempos
+t = 
+
+#Accion de control
+u = 
+
+#Datos del tacometro (V -> rad/s) (Se asume relacion lineal por lo presentado en la tesis)
+k_tac = 30.79
+v_tac = 
+w = k_tac * v_tac 
+
+
+
+#Datos del potenciometro (V -> m) (Se asume relacion lineal)
+k_pot = 
+v_pot = 
+x = k_pot * v_pot
+
+
+
+
+
+#------ PARAMETROS DEL ARX (Se deben escalar los datos antes)
+#coeficientes de entrada
+na =
+
+#coeficientes de la salida
+nb = 
+
+
+
+
